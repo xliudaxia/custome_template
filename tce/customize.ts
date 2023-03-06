36aa74f1248241b307b895ae145953f0bbe8ddd7
liuwenhao <liuwenhao@live.com>
2023-03-06 22:13:59 +0800

    feat: add custom test files

---
+++ tce/customize.ts
@@ -0,0 +1,17 @@
+const isTCE = true
+
+
+let customize;
+if (isTCE) {
+    customize = {
+        name:'Tom',
+        age:25
+    }
+} else {
+    customize = {
+        name:'Jesse',
+        age:21
+    }
+}
+
+export default customize;
